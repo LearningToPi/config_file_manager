8467644239eeb33159a4718fdd80b8a6c206f0dc
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2026-09-12 22:03:26 -0700

    standardize naming in helper functions.  Set default table for mysql

--- src/config_file_manager/__init__.py
+++ src/config_file_manager/__init__.py
@@ -6,7 +6,7 @@ from .crypto import generate_encryption_key
 from .database import ConfigManagerDB
 
 
-VERSION = (1, 0, 3)    # updated 2026-03-19 20:55:05.854283 from : (1, 0, 2)
+VERSION = (1, 0, 4)    # updated 2026-09-12 21:51:46.665929 from : (1, 0, 3)
 
 
 def load_file(filename:str, encryption_key:bytes|None=None, encryption_key_file:str|None=None, log_level=INFO, save_on_change=False):
@@ -18,12 +18,12 @@ def load_file(filename:str, encryption_key:bytes|None=None, encryption_key_file:
     raise ValueError(f"Unsupported config file type for file '{filename}'")
 
 
-def load_sqlite3_db(file_name:str, table:str='kv', encryption_key:bytes|None=None, encryption_key_file:str|None=None, log_level=INFO):
+def load_sqlite3_db(filename:str, table:str='kv', encryption_key:bytes|None=None, encryption_key_file:str|None=None, log_level=INFO):
     ''' Load or create a SQLite3 database and return a ConfigManagerDB instance '''
-    return ConfigManagerDB(database=file_name, db_type='sqlite3', table=table, log_level=log_level, encryption_key=encryption_key, encryption_key_file=encryption_key_file)
+    return ConfigManagerDB(database=filename, db_type='sqlite3', table=table, log_level=log_level, encryption_key=encryption_key, encryption_key_file=encryption_key_file)
 
 
-def load_mysql_db(host:str, user:str, password:str, database:str, table:str, encryption_key:bytes|None=None, encryption_key_file:str|None=None, log_level=INFO):
+def load_mysql_db(host:str, user:str, password:str, database:str, table:str='kv', encryption_key:bytes|None=None, encryption_key_file:str|None=None, log_level=INFO):
     ''' Load a MySQL database and return a ConfigManagerDB instance '''
     return ConfigManagerDB(host=host, db_type='mysql',user=user, password=password, database=database, table=table, log_level=log_level, encryption_key=encryption_key,
                            encryption_key_file=encryption_key_file)
